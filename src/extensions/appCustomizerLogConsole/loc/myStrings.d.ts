343b167e8748b7961958e3386027b8d95678355d
CARLOS MARTIN PERAL <cmarti3@emea.corpdir.net>
2023-06-25 10:06:32 +0200

    Initial commit

---
+++ src/extensions/appCustomizerLogConsole/loc/myStrings.d.ts
@@ -0,0 +1,8 @@
+declare interface IAppCustomizerLogConsoleApplicationCustomizerStrings {
+  Title: string;
+}
+
+declare module 'AppCustomizerLogConsoleApplicationCustomizerStrings' {
+  const strings: IAppCustomizerLogConsoleApplicationCustomizerStrings;
+  export = strings;
+}
